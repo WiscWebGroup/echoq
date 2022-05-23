adfa494d65f99c9dd45da532ceacf29a36f9665a
NataRich <natarichjay@gmail.com>
2022-05-23 12:42:37 -0500

    add page transition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,24 @@ import Home from "./page/home/Home"
 import Account from "./page/account/Account"
 
 import "./app.css"
+import AlertProvider from "./components/alert/AlertProvider"
+import { ScaleFade } from "@chakra-ui/react"
 
 export const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </Router>
+      <AlertProvider>
+        <ScaleFade className="App" initialScale={0.9} in>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/account" element={<Account />} />
+            </Routes>
+          </Router>
+        </ScaleFade>
+      </AlertProvider>
     </div>
   )
 }
