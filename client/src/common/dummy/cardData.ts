bf9fbd80a5c34ce3b5399d5fbd12e43e934d5253
NataRich <natarichjay@gmail.com>
2022-05-25 08:38:45 -0500

    add dummy test data

---
+++ client/src/common/dummy/cardData.ts
@@ -0,0 +1,171 @@
+export const dummyCardData = [
+  {
+    order: 1,
+    question: "Pzca aqbxfr etw tvl wci qjbczlin rvj l qxrgbxjif?",
+    response:
+      "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+    askedAt: "2021-10-07 12:28:48",
+    respondedAt: "2021-12-26 01:16:43"
+  },
+  {
+    order: 2,
+    question: "Buyy dlvzmp ckt vti ztr ugpkkhrl bmo s gqpvjgsic?",
+    response:
+      "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum.",
+    askedAt: "2021-09-22 08:01:29",
+    respondedAt: "2022-04-16 01:26:01"
+  },
+  {
+    order: 3,
+    question: "Qvdz rkjadb szl wqk pfz zeyfyzai yae j kqrhbranf?",
+    response: "Donec semper sapien a libero. Nam dui.",
+    askedAt: "2021-07-07 03:52:51",
+    respondedAt: "2022-01-28 18:04:46"
+  },
+  {
+    order: 4,
+    question: "Lveq jguyeo goc yhh dnj gcajsfik oml n beehqpgor?",
+    response: "Aliquam erat volutpat. In congue. Etiam justo.",
+    askedAt: "2022-05-04 20:03:55",
+    respondedAt: "2021-08-12 20:03:09"
+  },
+  {
+    order: 5,
+    question: "Yjwb cupoja nty swv ren anhrbypo qyv p sjzblyqmt?",
+    response: "Cras pellentesque volutpat dui.",
+    askedAt: "2021-12-18 21:46:08",
+    respondedAt: "2022-05-12 02:33:18"
+  },
+  {
+    order: 6,
+    question: "Hxxp pewbes nkc hmi cfi muqtkicn ezg x jbaffigkp?",
+    response:
+      "Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    askedAt: "2021-06-08 01:19:46",
+    respondedAt: "2021-06-15 06:12:03"
+  },
+  {
+    order: 7,
+    question: "Qasp cngdhn nje coh ywk clmrfhvm iku t pnmwztdge?",
+    response:
+      "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    askedAt: "2021-11-08 13:26:17",
+    respondedAt: "2022-01-12 19:40:12"
+  },
+  {
+    order: 8,
+    question: "Auxl sqaywh lex huu uvt ogidvsoa jqk r pkpcvcvdu?",
+    response: "Phasellus id sapien in sapien iaculis congue.",
+    askedAt: "2021-12-11 21:52:23",
+    respondedAt: "2021-12-19 11:37:56"
+  },
+  {
+    order: 9,
+    question: "Sowa cljijc ntd yqb gdy rpikhfxd tny g xluxjyjzz?",
+    response:
+      "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum.",
+    askedAt: "2022-05-03 06:48:45",
+    respondedAt: "2022-05-14 16:38:03"
+  },
+  {
+    order: 10,
+    question: "Kfgm xwzmwv thr bnd rsk buuleqyl vek g xvujynxae?",
+    response:
+      "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.",
+    askedAt: "2021-10-06 06:53:34",
+    respondedAt: "2022-04-26 19:41:21"
+  }
+]
+
+export const dummyOwnerRCardData = [
+  {
+    order: 1,
+    question: "Qsj sogzc?",
+    response: "Nullam sit amet turpis elementum ligula vehicula consequat.",
+    show: true,
+    askedAt: "2022-05-01 15:25:23",
+    respondedAt: "2022-01-16 19:20:11"
+  },
+  {
+    order: 2,
+    question:
+      "Zqr urhru pkmiw x di byu aidxoz Zqr urhru pkmiw x di byu aidxoz?",
+    response:
+      "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis.",
+    show: true,
+    askedAt: "2021-12-12 00:48:07",
+    respondedAt: "2022-01-26 20:52:54"
+  },
+  {
+    order: 3,
+    question: "Mfe ucqdz bnven j yl off tspwzf?",
+    response:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    show: false,
+    askedAt: "2021-06-01 07:08:12",
+    respondedAt: "2022-02-05 18:01:39"
+  },
+  {
+    order: 4,
+    question: "Yfp rkdkn aalft g mk ovd jtqokg?",
+    response:
+      "Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+    show: false,
+    askedAt: "2021-05-30 07:50:03",
+    respondedAt: "2021-07-27 03:38:23"
+  },
+  {
+    order: 5,
+    question: "Het ilefa ndbvy j uk nkc rfqcgz?",
+    response:
+      "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat.",
+    show: true,
+    askedAt: "2021-05-25 02:53:59",
+    respondedAt: "2021-09-16 02:00:19"
+  },
+  {
+    order: 6,
+    question: "Ehe vzdhl zekwg s nx gjs uthyvw?",
+    response:
+      "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
+    show: false,
+    askedAt: "2021-10-01 04:12:59",
+    respondedAt: "2022-01-27 09:49:59"
+  },
+  {
+    order: 7,
+    question: "Gft ucqdr ftzar j bb drm uomlcr?",
+    response:
+      "Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque.",
+    show: true,
+    askedAt: "2021-06-11 16:49:27",
+    respondedAt: "2021-06-05 00:49:06"
+  },
+  {
+    order: 8,
+    question: "Lmn abqbk vfmeu q sk lkc jfklfq?",
+    response:
+      "Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy.",
+    show: true,
+    askedAt: "2022-03-17 05:45:04",
+    respondedAt: "2021-07-22 17:57:44"
+  },
+  {
+    order: 9,
+    question: "Jcy igbhs srouw j iv xfs buwbvb?",
+    response:
+      "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue.",
+    show: true,
+    askedAt: "2022-02-19 09:43:01",
+    respondedAt: "2021-09-09 13:31:18"
+  },
+  {
+    order: 10,
+    question: "Hsz qzqeo pjotp a nb jri jxxryx?",
+    response:
+      "Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo.",
+    show: false,
+    askedAt: "2022-01-01 09:23:07",
+    respondedAt: "2022-04-04 12:30:01"
+  }
+]
